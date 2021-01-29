049e00d0c7f090b3b724516a472fb922aad5f285
Francis Albuna <falbuna1@gmail.com>
2021-01-29 10:46:32 -0800

    Installed tailwindcss for the project.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
+import 'tailwindcss/dist/tailwind.min.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
