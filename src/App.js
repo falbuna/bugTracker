bed09a9868fa53544045f6faff761425251ce29e
Francis Albuna <falbuna1@gmail.com>
2021-02-01 09:23:46 -0800

    Made the sidebar and navbar a component. Created a dashboard page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 // import LoginButton from './component/LoginButton';
-// import LogoutButton from './component/LogoutButton';
+import LogoutButton from './component/LogoutButton';
 // import Profile from './component/Profile';
 import Login from './pages/Login';
+import Sidebar from './component/Sidebar'
 import Home from './pages/Home';
 import { useAuth0 } from '@auth0/auth0-react'
+import Navbar from './component/Navbar';
 
 function App() {
 
@@ -16,9 +18,13 @@ function App() {
   return (
     <>
       <Login />
+      <Navbar />
+      <div className="h-screen flex overflow-hidden bg-white">
+      <Sidebar />
       <Home />
-      {/* <LogoutButton /> */}
+      <LogoutButton />
       {/* <Profile /> */}
+      </div>
     </>
   );
 }
